09d427da4ef73aaf3454ae48a37c1cc5d9760f6d
Luke Segars <luke@theironyard.com>
2016-03-10 12:52:29 -0500

    Today's JS.

---
+++ daily/09/daily.js
@@ -0,0 +1,19 @@
+/**
+ * Write a function that accepts a string as a parameter and
+ * returns true if the string starts with 'g' and ends with 's',
+ * otherwise returns false.
+ */
+
+function namecheck(name) {
+
+}
+
+// 'gregs' should be true
+// 'flower' should be false
+// 'glower' should be false
+// 'tags' should be false
+
+console.log("namecheck('gregs') = ", namecheck('gregs'));
+console.log("namecheck('flower') = ", namecheck('flower'));
+console.log("namecheck('glower') = ", namecheck('glower'));
+console.log("namecheck('tags') = ", namecheck('tags'));
