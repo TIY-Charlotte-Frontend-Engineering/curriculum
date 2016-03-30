fbd88334642c37f4a501fcbafea828391670503c
Luke Segars <luke@theironyard.com>
2016-03-30 11:15:17 -0400

    Food radio button example.

---
+++ daily/23/foods/js/foods.js
@@ -0,0 +1,21 @@
+module.exports = [{
+    id: 0,
+    name: 'Broccoli',
+    type: 'Vegetable',
+    suggestions: ['broccoli casserole', 'broccoli cheddar soup'],
+}, {
+    id: 1,
+    name: 'Asparagus',
+    type: 'Vegetable',
+    suggestions: ['asparagus casserole', 'asparagus soup'],
+}, {
+    id: 2,
+    name: 'Venison',
+    type: 'Meat',
+    suggestions: ['venison burgers', 'venison cheddar soup']
+}, {
+    id: 3,
+    name: 'Onion',
+    type: 'Vegetable',
+    suggestions: ['onion stew', 'steak and onions'],
+}];
