d81890f3622931a9ab9e141012aebbf253a07b01
Luke Segars <luke@theironyard.com>
2016-09-06 11:59:20 -0400

    Regular expressions.

---
+++ daily/26/regex.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * ^    Start of string
+ * $    End of string
+ * 
+ * []   Character sets
+ * ()   Groups
+ * 
+ * COUNTS
+ * ?    Matches the preceding expression 0 or 1 time.
+ * *    Matches the preceding expression 0 or more times.
+ * +    Matches the preceding expression 1 or more times.
+ * {}   Range
+ */
