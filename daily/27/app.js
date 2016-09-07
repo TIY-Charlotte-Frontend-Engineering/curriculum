7514047e57585a46e2159d5025cd152a32009bf0
Luke Segars <luke@theironyard.com>
2016-09-07 13:54:28 -0400

    Content from today.

---
+++ daily/27/app.js
@@ -0,0 +1,52 @@
+let _ = require('lodash');
+let $ = require('jquery');
+
+/*
+function isHuman(person) {
+    if (person.length > 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
+let moderator_name = 'Shelby';
+
+window.addEventListener('load', function () {
+    $('body').append('<h2>Comment by ' + moderator_name + '</h2>');
+    $('li').on('click', function () {
+        console.log('you clicked er');
+    })
+
+
+
+
+
+
+    // console.log('oh boy');
+
+/*
+    // Range gives us numbers from (lower to upper)
+    let nums = _.range(1, 101);
+
+    let folks = ['Gladys', 'Jeb', 'Franklin', 'Cletus'];
+    // Does EVERY element of folks return true?
+    let allArePeople = _.every(folks, isHuman);
+    // Do SOME of these elements return true?
+    let anyArePeople = _.some(folks, isHuman);
+
+    // sample
+
+    // shuffle
+    // partition
+    let doods = [1, 5, 6, 7, 8, 9];
+    let splitz = _.partition(doods, function (num) {
+        if (num > 5) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    console.log(splitz);
+    */
+});
