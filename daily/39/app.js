33e78900c8fe95b9e1f1df5ee740e85fea413e04
Luke Segars <luke@theironyard.com>
2016-09-23 15:03:19 -0400

    Created a <book> component.

--- daily/39/app.js
+++ daily/39/app.js
@@ -1,5 +1,14 @@
 let app = angular.module('BookApp', []);
 
+// Name of component must match name of HTML tag
+app.component('book', {
+    templateUrl: 'components/book.html',
+    // When using a <book>, users can specify readable="???"
+    bindings: {
+        readable: '<',
+    },
+});
+
 app.controller('CheckoutController', function ($scope, BookService) {
     $scope.books = BookService.getBooks();
     $scope.username = '';
