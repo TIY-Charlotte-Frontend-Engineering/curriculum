dce5b9f7c25e3fe1dc8bc4c090fe945399673747
Luke Segars <luke@lukesegars.com>
2017-02-02 11:57:46 -0500

    Promise.

---
+++ daily/40-finale/geocaching.js
@@ -0,0 +1,14 @@
+/**
+ * Geocaching app
+ * 
+ * 1. Users can add a new item to the database.
+ * 2. Users can provide a location (Charlotte, NC) and get a list of 
+ *    caches near them.
+ * 3. When users find something, they can mark it as found.
+ * 
+ * There should be a way for users:
+ *  - to add an item
+ *  - to see what items are nearby
+ *  - to see what items they've already found
+ */
+
