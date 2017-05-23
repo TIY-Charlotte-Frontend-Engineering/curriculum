7805c7fc14f7cb12dbadafaa45d1279b4c714a65
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-05-23 09:32:31 -0400

    Debugged

--- fixes/vowels.js
+++ fixes/vowels.js
@@ -1,32 +1,22 @@
-/**
- * Write a function that removes all vowels from a sentence.
- * 
- * Tip: figure out how to solve a smaller part. Solve the
- * smaller part multiple times to solve the larger problem.
- */
+
 
 function removeVowels(sentence) {
-    // Every character is or is not a vowel (boolean)
-    // Loop over all letters
-    // If 
+
     let letters = [];
 
-    for (let i = 0; i < 5; i++) {
-        // If its a vowel
-        // if (['a', 'e', 'i', 'o', 'u'].indexOf(sentence[i]) > -1) {
-
-        // }
-        if (sentence[i] !== 'a' && 
-            sentence[i] !== 'e' &&
-            sentence[i] !== 'i' &&
-            sentence[i] !== 'o' && 
-            sentence[i] !== 'u') {
+    for (let i = 0; i < sentence.length; i++) {
+  
+        if (sentence[i].toLowerCase() !== 'a' && 
+            sentence[i].toLowerCase() !== 'e' &&
+            sentence[i].toLowerCase() !== 'i' &&
+            sentence[i].toLowerCase() !== 'o' && 
+            sentence[i].toLowerCase() !== 'u') {
 
             letters.push(sentence[i]);
         }
     }
 
-    return letters;
+    return letters.join('');
 }
 
 console.log(removeVowels('What does this even mean?'));
