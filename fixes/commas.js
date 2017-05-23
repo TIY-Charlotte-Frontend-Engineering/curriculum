029fe782eb07236341c2be6ce8ae1d403bffa8e5
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-05-23 09:44:50 -0400

    commas fix

--- fixes/commas.js
+++ fixes/commas.js
@@ -9,22 +9,30 @@
  * commas(1843583) // returns '1,843,583'
  */
 
+import test from 'ava'; 
+
 function commas(num) {
     // 1. convert to string, then split to array
     // 2. start at the back, splice in a comma every third spot
     // 3. join back into a string and return
 
     // 2
-    for (let i = num.length - 3; i > 0; i = i - 3) {
+    let str = num.split(''); 
+
+    for (let i = str.length - 3; i > 0; i = i - 3) {
         // splice deletes stuff (second argument) and adds stuff (third and beyond)
-        num.splice(i, 0, ',');
+        str.splice(i, 0, ',');
     }
 
     // 3
-    return num.join('');
+    return str.join('');
 }
 
 console.log(commas(1000))
 console.log(commas(42000))
 console.log(commas(1843583))
 console.log(commas(231843583))
+
+test('comma', function (t) {
+    t.deepEqual(commas(1000), 1,000);
+}); 
